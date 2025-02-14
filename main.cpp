45a775d5d0bf6497c4ab2dd421540736bd2ccbd3
Karl-Mitx <kescobarc14@miumg.edu.gt>
2025-02-14 17:52:39 -0600

    Inicio de la tarea de ejercicios en C++

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    cout << "Escribir un programa que pida al usuario su edad y determine si es mayor de edad o no.";
+    
+    return 0;
+}
+
+
+
