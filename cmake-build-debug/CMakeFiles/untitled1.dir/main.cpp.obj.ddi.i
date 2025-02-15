30da896331d5aedc00029856bca60819bcdd0d4a
Karl-Mitx <kescobarc14@miumg.edu.gt>
2025-02-14 18:59:55 -0600

    Algoritmo que detecta un numero mayor, menor o igual

--- cmake-build-debug/CMakeFiles/untitled1.dir/main.cpp.obj.ddi.i
+++ cmake-build-debug/CMakeFiles/untitled1.dir/main.cpp.obj.ddi.i
@@ -41496,5 +41496,30 @@ int main() {
     } else {
             cout << "Usted es menor de edad" << endl;
     }
+
+    cout << endl;
+    int a;
+    int b;
+    cout << "Escriba dos numeros y se determinara cual es el mayor de los dos";
+    cout << endl;
+    cout << "Escriba el primer numero: ";
+    cin >> a;
+    cout << endl;
+    cout << "Escriba el segundo numero: ";
+    cin >> b;
+    if (a > b) {
+        cout << "El numero " << a << " es mayor de los dos numeros";
+    } else {
+        if (a < b) {
+            cout << "El numero " << a << " es menor de los dos numeros";
+        }
+        else {
+            if (a == b) {
+                cout << "Los dos numeros son iguales";
+            }
+        }
+    }
+
+
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-
+// Escribir un programa que pida al usuario su edad y determine si es mayor de edad o no.
     cout << "Escribir un programa que pida al usuario su edad y determine si es mayor de edad o no.";
     int edad;
     cout << endl;
@@ -14,6 +14,30 @@ int main() {
     } else {
             cout << "Usted es menor de edad" << endl;
     }
+    // Escribir un programa que pida al usuario dos números y determine cuál es el mayor de los dos.   cout << endl;
+    int a;
+    int b;
+    cout << "Escriba dos numeros y se determinara cual es el mayor de los dos";
+    cout << endl;
+    cout << "Escriba el primer numero: ";
+    cin >> a;
+    cout << endl;
+    cout << "Escriba el segundo numero: ";
+    cin >> b;
+    if (a > b) {
+        cout << "El numero " << a << " es mayor de los dos numeros";
+    } else {
+        if (a < b) {
+            cout << "El numero " << a << " es menor de los dos numeros";
+        }
+        else {
+            if (a == b) {
+                cout << "Los dos numeros son iguales";
+            }
+        }
+    }
+
+
     return 0;
 }
 
