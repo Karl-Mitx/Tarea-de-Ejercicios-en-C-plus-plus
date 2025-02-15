0af60185006085b790e56ad931f2eb0dae1c02a9
Karl-Mitx <kescobarc14@miumg.edu.gt>
2025-02-14 18:43:16 -0600

    Algoritmo que verifica mayoria de edad

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main() {
 
     cout << "Escribir un programa que pida al usuario su edad y determine si es mayor de edad o no.";
-    
+    int edad;
+    cout << endl;
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+    if (edad > 18) {
+        cout << "Usted es mayor de edad" << endl;
+    } else {
+            cout << "Usted es menor de edad" << endl;
+    }
     return 0;
 }
 
